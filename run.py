7ac92c7005c66f2db7e2fbf9178af01257f5b9a7
Katya <me@katyagordeeva.com>
2018-10-01 11:54:52 +0200

    added a function to explore available data and moved testing processing of a goal to a separate function

--- run.py
+++ run.py
@@ -8,19 +8,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 API_URL = "https://www.rescuetime.com/anapi/data"
-LIMIT_ENTERTAINMENT_HOURS = 0.5
+LIMIT_ENTERTAINMENT_HOURS = 0
+EXPLORE=False
 
-def get_entertainment_time(data):
-	# looking for entertainment in raw response
-	for row in data:
-		if "Entertainment" in row:
-			# converting seconds to hours
-			return row[1]/60/60
-
-def send_notification(threshold):
+def send_notification(threshold, current_value):
 	# testing sending a desktop notification
 	# icons' spec: https://specifications.freedesktop.org/icon-naming-spec/icon-naming-spec-latest.html
-	p = subprocess.call(["notify-send", "RescueTime Alert", "Ooops, you've spent more than %d for Entertainment today. Close Netflix and get your lazy ass to surf!" % threshold, "--icon=weather-severe-alert"])
+	p = subprocess.call(["notify-send", "RescueTime Alert", "Ooops, you've spent %d minutes on Entertainment today. Close Netflix and get your lazy ass to surf!" % current_value, "--icon=weather-severe-alert"])
 
 def plot_time_category(data):
 	# testing some visualizations
@@ -33,31 +27,58 @@ def plot_time_category(data):
 	cleaned.plot.bar(x="Category", y="Time")
 	plt.show()
 
+def explore_data(token):
+	params = {
+		"key": token,
+		"format": "json",
+		"perspective": "interval", # interval|rank (default)
+		"resolution_time": "week", # minute|hour|day|week|month (works only for perspective=interval)
+		"restrict_kind": "category", # overview|category|activity|document|efficiency|productivity
+		# if not restrict_time is provided, API returns log for the current day
+		"restrict_begin": "2018-09-20",
+		# "restrict_end": "2018-09-29",
+		"restrict_thing": "Uncategorized"
+	}
+
+	r = requests.get(API_URL, params=params)
+	data = r.json()
+	print(data)
+	df = pd.DataFrame(data['rows'], columns=data['row_headers'])
+	print(df)
+
+def process_entertainment_time_today(token):
+	# looking for entertainment in raw response
+
+	params = {
+		"key": token,
+		"format": "json",
+		"perspective": "rank",
+		"restrict_kind": "overview"
+	}
+
+	r = requests.get(API_URL, params=params)
+	today = r.json()
+
+	plot_time_category(today)
+
+	for row in today['rows']:
+		if "Entertainment" in row:
+			# converting seconds to hours
+			current_value_hours = row[1]/60/60
+			if current_value_hours > LIMIT_ENTERTAINMENT_HOURS:
+				send_notification(LIMIT_ENTERTAINMENT_HOURS, current_value_hours*60)
+
 def main():
 
 	with open('config.json', 'r') as file:
 		config = json.loads(file.read())
 		token = config['API_TOKEN']
 
-	params = {
-		"key": token,
-		"format": "json",
-		# "perspective": "interval", # interval|rank
-		"resolution_time": "minute", # minute|hour|day|week|month
-		"restrict_kind": "overview" # overview|category|activity|productivity
-		# if not restrict_time is provided, API returns log for the current day
-		# "restrict_begin": "2018-09-29",
-		# "restrict_end": "2018-09-29"
-	}
+	if EXPLORE:
+		explore_data(token)
+		quit()
 
-	# documentation: https://www.rescuetime.com/anapi/setup/documentation#analytic-api-reference
-	r = requests.get(API_URL, params=params)
-	today = r.json()
-
-	plot_time_category(today)
-
-	if get_entertainment_time(today["rows"]) > LIMIT_ENTERTAINMENT_HOURS:
-		send_notification(LIMIT_ENTERTAINMENT_HOURS)
+	process_entertainment_time_today(token)
 
 if __name__ == "__main__":
 	main()
