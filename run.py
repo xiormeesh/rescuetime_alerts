476c99077aeb0a6d7aefcc9fd5e121b0ca3a888d
Katya <me@katyagordeeva.com>
2018-10-20 17:31:11 +0200

    solved bug with being overproductive resulting in negative remaining time :)

--- run.py
+++ run.py
@@ -37,11 +37,11 @@ def process_category(config, task):
 		# send notification
 		if task['task_type'] == 'limit':
 			if time_spent > task['minutes']:
-				utils.send_notification("Ooops you've spent %d minutes out of %d limit on %s today." \
+				utils.send_notification("Ooops you've spent %d minutes out of %d minutes limit on %s today." \
 					% (time_spent, task['minutes'], task['slice_name']))
 		elif task['task_type'] == 'goal':
 			if time_spent < task['minutes']:
-				utils.send_notification("Work harder! You still have %d minutes out of %d goal ahead of you to work on %s" \
+				utils.send_notification("Work harder! You have %d minutes out of %d minutes goal ahead of you to work on %s" \
 					% (task['minutes'] - time_spent, task['minutes'], task['slice_name']))
 
 def process_productivity(config, task):
@@ -49,12 +49,14 @@ def process_productivity(config, task):
 	productivity_score = today.at[0, "Efficiency (percent)"]
 
 	today = utils.send_request(config, perspective="interval", resolution_time="day", restrict_kind="productivity")
-	very_productive_min = utils.extract_from_cell(today, 'Time', 'Productivity', 2)
-	productive_min = utils.extract_from_cell(today, 'Time', 'Productivity', 1)
+	very_productive_min = utils.extract_from_cell(today, 'Time', 'Productivity', 2) or 0
+	productive_min = utils.extract_from_cell(today, 'Time', 'Productivity', 1) or 0
+	time_remaining = task['minutes'] - (very_productive_min + productive_min)
+	time_remaining = time_remaining if time_remaining > 0 else 0
 
 	utils.send_notification("You are %d percent productive so far!\n" \
-		"You still have %d minutes out of %d goal ahead of you."
-		% (productivity_score, task['minutes'] - (very_productive_min + productive_min), task['minutes']))
+		"You have %d minutes out of %d minutes goal ahead of you."
+		% (productivity_score, time_remaining, task['minutes']))
 
 def plot_productivity_today_by_hour(token):
 	"""Plots per hour: time logged and productivity score"""
